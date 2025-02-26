f869e90dd3ed4cbee3b880e41f91d58ebd7c6722
Archisman_Das <113133668+archis1405@users.noreply.github.com>
2025-02-27 04:06:12 +0530

    managed all the redux states

--- weather_application/src/redux/slices/ForecastSlice.ts
+++ weather_application/src/redux/slices/ForecastSlice.ts
@@ -35,7 +35,7 @@ const initialState : ForecastDataState = {
 export const fetchData = createAsyncThunk('data/fetchData', async () => {
     try{
         console.log(import.meta.env.VITE_API_KEY);
-        const apiUrl = `/forecast.json?key=${import.meta.env.VITE_API_KEY}&q=London&days=7&aqi=yes`;
+        const apiUrl = `/forecast.json?key=${import.meta.env.VITE_API_KEY}&q=Kolkata&days=7&aqi=yes`;
         console.log("API Request URL:", apiUrl);
         const response = await axiosInstance.get(apiUrl);
         console.log(response);
@@ -58,13 +58,38 @@ const forecastSlice = createSlice({
             state.status = 'success';
             console.log("action",action);
             
-            const {location} = action.payload;
+            const {location , forecast , current} = action.payload;
             if (state.data) {
+                // Setting Location
                 state.data.location.country = location?.country;
                 state.data.location.name = location?.name;
                 state.data.location.region = location?.region;
                 state.data.location.localtime = location?.localtime;
+
+                state.data.forecast = forecast.forecastday.map((foreCastItem:any) => {
+                    return{
+                        date:foreCastItem.date,
+                        avgtemp_c: foreCastItem.day.avgtemp_c,
+                        avgtemp_f: foreCastItem.day.avgtemp_f,
+                        condition: foreCastItem.day.condition.text,
+                    }
+                });
             }
+
+            // Setting Current Data
+            state.data.currentData.uv = current.uv;
+            state.data.currentData.wind_kph = current.wind_kph;
+            state.data.currentData.humidity = current.humidity;
+            state.data.currentData.vis_km = current.vis_km;
+            state.data.currentData.aqi = current.air_quality.pm2_5;
+            state.data.currentData.sunrise = forecast.forecastday[0].astro.sunrise;
+            state.data.currentData.sunset = forecast.forecastday[0].astro.sunset;
+            state.data.currentData.temp_c = current.temp_c;
+            state.data.currentData.temp_f = current.temp_f;
+            state.data.currentData.condition = current.condition.text;
+            state.data.currentData.is_day = current.is_day;
+            state.data.currentData.chance_of_rain = forecast.forecastday[0].day.daily_chance_of_rain;
+
 
         })
         .addCase(fetchData.pending,(state) => {
