9f96a9648bfbcb197b35e2730a234564efcf68bb
Archisman_Das <113133668+archis1405@users.noreply.github.com>
2025-02-26 20:16:13 +0530

    Compleated the SideBar

--- weather_application/src/components/SideBar/Lowerhalf.tsx
+++ weather_application/src/components/SideBar/Lowerhalf.tsx
@@ -1,8 +1,46 @@
+import { FaDroplet } from "react-icons/fa6";
+import { FaSun } from "react-icons/fa";
+import { IoLocationSharp } from "react-icons/io5";
+
 function Lowerhalf(){
     return(
-        <div className= "h-[50%] w-full p-4">
-            LowerHalf
-        </div>
+        
+            <div className="h-[50%] w-full flex flex-col justify-between items-start">
+                <div className="flrx flex-col">
+                    {/*Precipitation Data*/}
+                    <div className="flex items-center gap-4 my-2 px-2 py-2 text-sm">
+                        <div>
+                            <FaSun />
+                        </div>
+                        <div className="fontsize-sm">
+                            SUNNY
+                        </div>
+
+                    </div>
+
+                    <div className="flex items-center gap-2 my-2 text-sm px-2 py-2">
+                        <div>
+                            {/* this will contain the droplet icon */}
+                            <FaDroplet />
+                        </div>
+                        <div className="fontsize-sm">
+                            PRECIPITATION - 10%
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex gap-2 items-center px-2 py-2 text-sm">
+                    {/*Location Data*/}
+                    <div>
+                        <IoLocationSharp />
+                    </div>
+                    <div className="fontsize-sm">
+                        New-Delhi , New-Delhi , India
+                    </div>
+                    
+                </div>
+            </div>
+        
     )
 }
 
--- weather_application/src/components/SideBar/SideBar.tsx
+++ weather_application/src/components/SideBar/SideBar.tsx
@@ -6,6 +6,8 @@ function SideBar(){
     return(
         <div className="basis-3/12 flex flex-col justify-between items-center text-black h-[100%] rounded-tl-3xl rounded-bl-3xl" style={{background: "rgba(255,255,255,0.5)"}}>
             <Upperhalf />
+            <div className="h-[1px] w-[85%] mt-8 bg-white rounded-md">
+            </div>
             <Lowerhalf />
 
         </div>
