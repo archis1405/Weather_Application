4ce917d8f646a4d8a1424a1ac8dfee3eeabea07c
Archisman_Das <113133668+archis1405@users.noreply.github.com>
2025-02-27 11:46:05 +0530

    Added colour to text in Highlight Card

--- weather_application/src/components/HighLightCard.tsx
+++ weather_application/src/components/HighLightCard.tsx
@@ -1,4 +1,6 @@
-function HighLightCard({ footer, data , title } : { footer: string, data: string, title: string }) {
+import { JSX } from "react";
+
+function HighLightCard({ footer, data , title } : { footer: string | JSX.Element , data: string, title: string | JSX.Element }) {
     return(
         <div className="flex flex-col justify-between items-center border bg-white rounded-3xl p-2 h-[13rem] w-[18rem]">
             <div className="text-lg font-semibold w-full text-left ml-8 mt-2 text-gray-400">
--- weather_application/src/components/MainData/HighlightRow.tsx
+++ weather_application/src/components/MainData/HighlightRow.tsx
@@ -5,17 +5,97 @@ import ReduxState from "../../interfaces/ReduxState";
 function HighlightRow(){
 
     const currentData = useSelector((state:ReduxState) => state.forecast.data.currentData);
-    
+
+    function uvRange():string{
+        if(currentData.uv <=5)
+            return "LOW";
+        else if( currentData.uv > 5 && currentData.uv <=8)
+            return "MODERATE";
+        else if(currentData.uv >=8)
+            return "HIGH";
+        return "Unknown";
+    }
+
+    function humidityRange():string{
+        if(currentData.humidity <=50)   
+            return "LOW";
+        else if(currentData.humidity > 50 && currentData.humidity <=80)
+            return "MODERATE";
+        else
+            return "HIGH";   
+    }
+
+    function visibilityRange():string{
+        if(currentData.vis_km <=5)
+            return "LOW";
+        else if(currentData.vis_km >5 && currentData.vis_km <=10)
+            return "MODERATE";
+        else
+            return "HIGH";
+    }
+
+    function aqiRange():string{
+        if(currentData.aqi <=100)
+            return "GOOD";
+        else if(currentData.aqi >100 && currentData.aqi <=200) 
+            return "MODERATE";
+        else if(currentData.aqi >200 && currentData.aqi <=300) 
+            return "POOR";
+        else if(currentData.aqi >300 && currentData.aqi <=400) 
+            return "VERY POOR";
+        else 
+            return "SEVERE";
+        
+    }
+
+    function getTextColor(range: string): string {
+        switch (range) {
+            case "LOW":
+                return "text-green-500 text-lg"; 
+            case "MODERATE":
+                return "text-yellow-500 text-semibold text-lg"; 
+            case "HIGH":
+                return "text-red-500 text-semibold text-lg"; 
+            case "GOOD":
+                return "text-green-500 text-semibold text-lg"; 
+            case "POOR":
+            case "VERY POOR":
+                return "text-orange-500 text-semibold text-lg"; 
+            case "SEVERE":
+                return "text-red-700 text-semibold text-lg"; 
+            default:
+                return "text-gray-500 text-semibold text-lg"; 
+        }
+    }
+
     return(
 
         
         <div className="flex flex-wrap gap-6 px-12 py-2">
-            <HighLightCard title="UV Index" data={currentData.uv.toString()} footer=""/>
-            <HighLightCard title="WIND STATUS" data={currentData.wind_kph.toString()} footer="kmph"/>
-            <HighLightCard title="HUMIDITY" data={currentData.humidity.toString()} footer=""/>
-            <HighLightCard title="VISIBILITY" data={currentData.vis_km.toString()} footer=""/>
-            <HighLightCard title="SUNRISE" data={currentData.sunrise.toString()} footer={currentData.sunset.toString()}/>
-            <HighLightCard title="AIR QUALITY" data={currentData.aqi.toString()} footer=""/>
+            <HighLightCard title="UV Index" 
+                           data={currentData.uv.toString() || "N/A"}
+                           footer={<span className={getTextColor(uvRange())}>{uvRange()}</span>} 
+            />
+            <HighLightCard title="WIND STATUS" 
+                           data={currentData.wind_kph.toString() || "N/A"}
+                           footer="kmph " 
+            />
+            <HighLightCard title="HUMIDITY" 
+                           data={currentData.humidity.toString() || "N/A"} 
+                           footer={<span className={getTextColor(humidityRange())}>{humidityRange()}</span>} 
+            />
+            <HighLightCard title="VISIBILITY" 
+                           data={currentData.vis_km.toString() || "N/A"} 
+                           footer={<span className={getTextColor(visibilityRange())}>{visibilityRange()}</span>} 
+            />
+            <HighLightCard title="SUNRISE" 
+                           data={currentData.sunrise.toString() || "N/A"} 
+                           footer={currentData.sunset.toString() || "N/A"}
+            />
+            <HighLightCard title="AIR QUALITY" 
+                           data={currentData.aqi.toString() || "N/A"} 
+                           footer={<span className={getTextColor(aqiRange())}>{aqiRange()}</span>} 
+            />
             
         </div>
     )
